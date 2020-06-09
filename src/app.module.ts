9e8fcfb2c64f361b00586c7da4704eb90d18f895
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2020-06-10 00:32:24 +0200

    Cats Module
      - Created Cats module and updated properly app main module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
