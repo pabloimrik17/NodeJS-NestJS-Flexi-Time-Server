7b0ab90d32ca7c7b0bb28f8842500536b577c779
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2020-07-09 23:03:59 +0200

    Added Procfile config to start correctly the server after is deployed on Heroku

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,22 +3,28 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-// describe('AppController (e2e)', () => {
-//   let app: INestApplication;
-//
-//   beforeEach(async () => {
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     }).compile();
-//
-//     app = moduleFixture.createNestApplication();
-//     await app.init();
-//   });
-//
-//   it('/ (GET)', () => {
-//     return request(app.getHttpServer())
-//       .get('/')
-//       .expect(200)
-//       .expect('Hello World!');
-//   });
-// });
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it.skip('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+
+  it('Sample', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(404);
+  });
+});
