cb6854ab49464187724d4ce20c354b1860168368
Etherless <pabloimrik17@gmail.com>
2020-07-19 16:43:52 +0200

    Added swagger api tag to app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Default')
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
