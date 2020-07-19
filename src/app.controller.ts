9f3a59f64971347b43e598a1179d9f9eace959e1
Etherless <pabloimrik17@gmail.com>
2020-07-19 14:59:30 +0200

    Swagger
      - added swagger support and basic config
    
    Typescript
    
      - added json file import support to config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Default')
 @Controller()
 export class AppController {
   @Get()
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version as projectVersion } from '../package.json';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocumentOptions = new DocumentBuilder()
+    .setTitle('Flexi Time Server')
+    .addBearerAuth()
+    .setDescription('Flexi Time Server API description')
+    .setVersion(projectVersion)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerDocumentOptions);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
